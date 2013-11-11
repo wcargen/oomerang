0c7ac5f5f1e435203ee679085678faac81911162
Imogen <imogen.wentworth@gmail.com>
2013-11-11 11:13:14 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oomerang::Application.config.secret_token = '5a2a90a341a6fd1f55ef22ca28b15736b8fd7b0dcfa120a1778a0ec4247eb930d820158b2905eea89517094f4f782e8512ca93212f86854d86924d0a7e2447c1'
