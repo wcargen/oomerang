1243dd17e0fe9332b8ff6ed88bbbd3de2bba9230
wcargen <wcargen@gmail.com>
2013-11-14 13:12:50 -0800

    Adding javascript/google maps files

---
+++ app/assets/javascripts/jsFunctionBlocks1.js
@@ -0,0 +1,113 @@
+////////// If the browser doesn't support Geolocation, it renders the San Francisco Map
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(37.7749300, -122.4194200),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+///////// Displays the markers on the map
+function loadMarkers() {
+
+  //Erase markers from the map to display new ones
+  for(var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers=[];
+
+  //Get map bounds to define which markers get displayed
+  var bounds = map.getBounds();
+  for(var i=0;i<locations.length;i++){
+
+    if(locations[i]["lat"]>=0){
+      var latTopBound = bounds["fa"]["b"];
+      var latBotBound = bounds["fa"]["d"];
+    } else {
+      var latTopBound = bounds["fa"]["d"];
+      var latBotBound = bounds["fa"]["b"];
+    }
+
+    if(locations[i]["lng"]>=0){
+      var lngLeftBound = bounds["ga"]["b"];
+      var lngRightBound = bounds["ga"]["d"];
+    } else {
+      var lngLeftBound = bounds["ga"]["d"];
+      var lngRightBound = bounds["ga"]["b"];
+    }
+
+    //Check if markers fall inside the bounds
+    if((locations[i]["lat"]<latTopBound && locations[i]["lat"]>latBotBound) && (locations[i]["lng"]<lngLeftBound && locations[i]["lng"]>lngRightBound)){
+      if(locations[i]["type"] == "lost"){
+        var image = "/assets/MarkerPurple.png";
+      } else{
+        var image = "/assets/MarkerWhite.png";
+      }
+      var Latlng = new google.maps.LatLng(locations[i]["lat"],locations[i]["lng"]);
+      // If map just loaded, markers will be animated
+      if(markerflag){
+        var Marker = new google.maps.Marker({
+          position: Latlng,
+          animation: google.maps.Animation.DROP,
+          icon: image
+        });
+      } else{
+        var Marker = new google.maps.Marker({
+          position: Latlng,
+          icon: image
+        });
+      }
+      markers.push(Marker);
+    }
+  }
+  markerflag=false;
+
+  //Set style for the marker clusters
+  var clusterStyles = [
+    {
+      opt_textColor: 'white',
+      url: '/assets/MarkerPurple.png',
+      height: 43,
+      width: 27,
+      textSize: 1
+    },
+   {
+      opt_textColor: 'white',
+      url: '/assets/MarkerWhite.png',
+      height: 43,
+      width: 27,
+      textSize: 1
+    },
+   {
+      opt_textColor: 'white',
+      url: '/assets/MarkerPurple.png',
+      height: 43,
+      width: 27,
+      textSize: 1
+    }
+  ];
+  var mcOptions = {gridSize: 100, styles: clusterStyles, maxZoom: 16};
+  var markerCluster = new MarkerClusterer(map, markers, mcOptions);
+}
+
+///////// Add a new lost or found item
+function addNewItem(event){
+  var image = "/assets/MarkerGrey.png";
+  var marker = new google.maps.Marker({
+    position: event.latLng,
+    map: map,
+    draggable:true,
+    icon: image
+  });
+}
+
+
---
+++ app/assets/javascripts/jsFunctionsBlock2.js
@@ -0,0 +1,47 @@
+///////// Add autocomplete to Places Bar
+function autoComp() {
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+      map: map
+  });
+
+  infowindow.close();
+  marker.setVisible(false);
+  input.className = '';
+  var place = autocomplete.getPlace();
+  if (!place.geometry) {
+    // Inform the user that the place was not found and return.
+    input.className = 'notfound';
+    return;
+  }
+
+  // If the place has a geometry, then present it on a map.
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);
+  }
+  marker.setIcon(/** @type {google.maps.Icon} */({
+    url: "/assets/MarkerGrey.png",
+    height: 43,
+    width: 27,
+    textSize: 1,
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+  }));
+  marker.setPosition(place.geometry.location);
+  marker.setVisible(true);
+
+  var address = '';
+  if (place.address_components) {
+    address = [
+      (place.address_components[0] && place.address_components[0].short_name || ''),
+      (place.address_components[1] && place.address_components[1].short_name || ''),
+      (place.address_components[2] && place.address_components[2].short_name || '')
+    ].join(' ');
+  }
+
+  infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+  infowindow.open(map, marker);
+}
